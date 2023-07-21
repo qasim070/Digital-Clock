920c268c847a6279ac01713df10e65b5451b3b90
qasim070 <62395180+qasim070@users.noreply.github.com>
2023-07-21 12:36:02 +0500

    Added AM PM format

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 function update() {
     let mainElement = document.getElementById("clockMain");
     let dateInstance = new Date();
+    let format  ;
+
+    if(dateInstance.getHours() > 12){
+        format = "AM"
+    }else{
+        format = "PM";
+    }
+
 
     mainElement.innerHTML= "Last Sync: " + dateInstance.getDate() + "/"
     + (dateInstance.getMonth()+1)  + "/" 
     + dateInstance.getFullYear() + " <br> "  + 
     + dateInstance.getHours() + ":"  
     + dateInstance.getMinutes() + ":" 
-    + dateInstance.getSeconds();
+    + dateInstance.getSeconds() + " " +format;
 }
